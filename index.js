27e3c6ca155514e17c33493bb80655159d14bf67
Alextryfg <Alextryfg>
2025-05-23 13:14:07 +0200

    done

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+//Definimos un array para almacenar las urls
+const urls = [];
+
+//logger middleware
+app.use(function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+}
+);
+
+//Añadimos linea para permitir a express recibir datos en formato json enviados por formulario
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -22,3 +36,47 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+//Se puede usar el req.body para obtener los parámetros de la url
+app.post('/api/shorturl', function (req, res) {
+  let original_url = req.body.url;
+
+  // Validar la URL
+  const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (!urlPattern.test(original_url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Aquí puedes agregar la lógica para almacenar la URL original y generar un short URL
+  // Por simplicidad, solo devolveremos la URL original en este ejemplo
+  // En un caso real, deberías almacenar la URL original en una base de datos y generar un ID único
+  // para la URL corta.
+  // Por ejemplo, podrías usar un contador para generar un ID único
+  // y luego almacenar la URL original en un objeto o base de datos con ese ID.
+  // En este caso, simplemente devolveremos la URL original como un ejemplo.
+
+  const shortUrl = urls.length + 1; // Generar un ID único para la URL corta
+  const newUrl = {
+    original_url: original_url,
+    short_url: shortUrl
+  };
+  urls.push(newUrl); // Almacenar la URL original y el ID en un array (o base de datos)
+  res.json(newUrl); // Devolver la URL original y el ID en la respuesta
+
+});
+
+app.get('/api/shorturl/:shorturl', function (req, res) {
+  const shortUrl = req.params.shorturl;
+  const url = urls.find(url => url.short_url == shortUrl);
+
+  //Si no se encuentra la url, se devuelve un error
+  if (!url) {
+    return res.json({ error: 'No short URL found for the given input' });
+  }
+
+  //Redirijo a la url original
+  res.redirect(url.original_url);
+
+  
+});
+
